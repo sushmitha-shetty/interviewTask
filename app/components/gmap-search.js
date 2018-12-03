cb0a6f51fda74f8d62778f027b6e5ed142012243
Sushmitha Shetty <sushmitha.shetty9@gmail.com>
2018-12-03 22:37:32 +0530

    ember cart

--- app/components/gmap-search.js
+++ app/components/gmap-search.js
@@ -1,11 +1,13 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+	city: '',
 	searchTextField: function() {
     var options = {
       types: ['(cities)']
     };
     var input = this.$('.search-field')[0];
     new window.google.maps.places.Autocomplete(input, options);
+    console.log(new window.google.maps.places.Autocomplete(input, options));
   }.on('didInsertElement')
 });
--- app/controllers/checkout.js
+++ app/controllers/checkout.js
@@ -1,5 +1,18 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  city: null
+  city: '',
+  placeOrder: false,
+  firstName: '',
+  lastName: '',
+  firstAdress: '',
+  secondAdress: '',
+  postCode: '',
+
+  actions: {
+    checkOut() {
+      this.set('placeOrder', true);
+      console.log("here!!!", this.get('firstName'));
+    }
+  }
 });
